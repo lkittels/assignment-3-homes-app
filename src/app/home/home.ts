f7bd5d562dce5bcd9623dc70c67b69f76a8fe38d
lkittels <lkittels@outlook.com>
2026-03-14 20:40:25 -0500

    Added the serach feature the the angular app

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -12,10 +12,24 @@ import { HousingService } from '../housing-service';
 export class Home {
   // readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
   housingLocationList: Housinglocationinfo[] = [];
+
   // Injecting HousingService into the Component
   housingService: HousingService = inject(HousingService);
+
+  filteredLocationList: Housinglocationinfo[] = [];
+
   constructor() {
-    //setting housingLocationlist to the data array in HousingService
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
   }
 }
